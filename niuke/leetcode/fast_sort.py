4c6902c68e637ec4ee4712470e9638d2b34bf340
MR-Fclef <1121181304@qq.com>
2018-04-17 23:07:08 +0800

    ..

---
+++ niuke/leetcode/fast_sort.py
@@ -0,0 +1,21 @@
+def quick_sort(array,l,r):
+	if l<r:
+		q = partition(array,l,r)
+		quick_sort(array,l,q-1)
+		quick_sort(array,q+1,r)
+	# print(array)
+	return array
+
+def partition(array,l,r):
+	x= array[r]
+	i = l-1
+	for j in range(l,r):
+		if array[j] <= x:
+			i += 1
+			array[i],array[j] = array[j],array[i]
+	array[i+1],array[r] = array[r],array[i+1]
+	print(i+1)
+	return i+1
+if __name__ == '__main__':
+	s = [3,6,5,8,9,4]
+	print(quick_sort(s,l=0,r = len(s)-1))
