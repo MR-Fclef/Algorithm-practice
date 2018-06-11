0043bd6143ba83492888ff7aa1b30830fdb0e721
MR-Fclef <1121181304@qq.com>
2018-06-11 13:40:37 +0800

    "inorder_postorder_traversal"

---
+++ niuke/leetcode/hash_Table/Construct_Binary_Tree_from_Inorder_Postorder_Traversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    # @param inorder, a list of integers
+    # @param postorder, a list of integers
+    # @return a tree node
+    def buildTree(self, inorder, postorder):
+        if len(inorder) == 0:
+            return None
+        if len(inorder) == 1:
+            return TreeNode(inorder[0])
+        root = TreeNode(postorder[len(postorder) - 1])
+        index = inorder.index(postorder[len(postorder) - 1])
+        root.left = self.buildTree(inorder[ 0 : index ], postorder[ 0 : index ])
+        root.right = self.buildTree(inorder[ index + 1 : len(inorder) ], postorder[ index : len(postorder) - 1 ])
+        return root
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+test = Solution()
+print(test.buildTree(inorder,postorder))
