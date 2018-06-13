17e1e176bd989ce2331071b5c57fd748ed406c14
MR-Fclef <1121181304@qq.com>
2018-06-13 23:30:25 +0800

    "path sum2"

--- niuke/leetcode/depth-first-search/path_Sum2.py
+++ niuke/leetcode/depth-first-search/path_Sum2.py
@@ -6,21 +6,31 @@ class TreeNode(object):
         self.right = None
 
 class Solution(object):
-    re = []
-    arr = []
-    def pathSum(self, root, sum,arr):
+    pre = []
+    def pathSum(self, root, sum):
         """
         :type root: TreeNode
         :type sum: int
         :rtype: List[List[int]]
         """
-        arr.append(root.val)
+        def path(root,currsum,arr):
+            if root.left==None and root.right==None:
+                if currsum == sum: res.append(arr)
+            if root.left: path(root.left,currsum+root.left.val,arr+[root.left.val])
+            if root.right: path(root.right,currsum+root.right.val,arr+[root.right.val])
+
+        res = []
         if root == None:
-            return False
-        if root.left ==None and root.right ==None and (sum-root.val)==0:
-            return re.append(arr)
-        return self.hasPathSum(root.left,sum-root.val,arr) or self.hasPathSum(root.right,sum-root.val,arr)
-    
+            return []
+        path(root,root.val,[root.val])
+        return res
+    def preorder(self,root):
+        if root == None:
+            return
+        self.pre.append(root.val)
+        self.preorder(root.left)
+        self.preorder(root.right)
+        return self.pre
     def buildTree(self, inorder, postorder):
         if len(inorder) == 0:
             return None
@@ -31,4 +41,10 @@ class Solution(object):
         root.left = self.buildTree(inorder[ 0 : index ], postorder[ 0 : index ])
         root.right = self.buildTree(inorder[ index + 1 : len(inorder) ], postorder[ index : len(postorder) - 1 ])
         return root
-
+test = Solution()
+postorder = [7,2,11,4,13,5,1,4,8,5]
+inorder = [7,11,2,4,5,13,8,5,4,1]
+root = test.buildTree(inorder,postorder)
+print(test.preorder(root))
+result = test.pathSum(root,22)
+print(result)
