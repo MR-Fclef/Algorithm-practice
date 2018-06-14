c707bb8eebcfa429e04413b671d17cc885b7aae5
MR-Fclef <1121181304@qq.com>
2018-06-14 14:31:00 +0800

    "flattern binary tree to linked list"

--- niuke/leetcode/depth-first-search/Flatten_Binary_Tree_to_Linked_List.py
+++ niuke/leetcode/depth-first-search/Flatten_Binary_Tree_to_Linked_List.py
@@ -11,3 +11,16 @@ class Solution(object):
         :type root: TreeNode
         :rtype: void Do not return anything, modify root in-place instead.
         """
+        if root == None:
+            return
+        self.flatten(root.left)
+        self.flatten(root.right)
+        p = root
+        if p.left == None:
+            return
+        p = p.left
+        while p.right:
+            p = p.right
+        p.right = root.right
+        root.right = root.left
+        root.left = None
