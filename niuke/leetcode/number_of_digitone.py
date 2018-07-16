66140f31dd69487a6f8f99b64b61add1a2652f10
MR-Fclef <1121181304@qq.com>
2018-07-16 22:31:47 +0800

    "number of digit one"

---
+++ niuke/leetcode/number_of_digitone.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countDigitOne(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        a = 1
+        b = 1
+        while(n>0):
+            res += (n+8)/10 * a +(n % 10 == 1) * b
+            b += n % 10 * a
+            a *= 10
+            n /= 10
+        return res
