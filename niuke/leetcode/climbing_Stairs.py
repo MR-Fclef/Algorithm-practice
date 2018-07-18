efa251acebf6cddc6b84ea452b113aa45041be20
MR-Fclef <1121181304@qq.com>
2018-07-18 11:32:46 +0800

    "climbing stairs"

--- niuke/leetcode/climbing_Stairs.py
+++ niuke/leetcode/climbing_Stairs.py
@@ -14,3 +14,15 @@ class Solution:
         for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
         return dp[n]
+'''
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        pre = cur = 1
+        for i in xrange(1, n):
+            pre, cur = cur, pre+cur
+        return cur
+'''
